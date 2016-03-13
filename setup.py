aba5a2d1f2dcd04dddde50c34b98c1bf6a604d43
Nicolas Seys <seys.nicolas@gmail.com>
2016-03-13 23:02:36 +0100

    updated setup due to recent commit

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='becon',
     version='1.0',
     description='Password Manager',
     packages=find_packages(),
-    scripts=['src/becon.py'],
+    scripts=['becon'],
 )
